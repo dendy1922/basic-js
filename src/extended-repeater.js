bab7b0f5ff1bb9a749a5ce2884b6388ea0a16b50
dendy1922 <55779398+dendy1922@users.noreply.github.com>
2020-10-04 22:02:28 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let repeatTimes = options.repeatTimes || 1;
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  let value = (str === null) ? "null" : str.toString();
+  let additionSeparator = (options.additionSeparator === undefined) ? "|" : (options.additionSeparator === null) ? "null" : options.additionSeparator.toString();
+  let addition = (options.addition === undefined) ? "" : (options.addition === null) ? "null" : options.addition.toString();
+  let separator = (options.separator === undefined) ? "+" : (options.separator === null) ? "null" : options.separator.toString();
+
+
+  let temp = [];
+  let tempResult = "";
+  let resultArr = [];
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    temp.push(addition)
+  }
+  tempResult = value + temp.join(additionSeparator);
+
+  for (let i = 0; i < repeatTimes; i++) {
+    resultArr.push(tempResult)
+  }
+
+  return resultArr.join(separator);
 };
-  
