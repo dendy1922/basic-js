ee659acfbe611171c10b547eac67fce1a0ef9845
dendy1922 <55779398+dendy1922@users.noreply.github.com>
2020-10-05 19:11:40 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error()
+  }
+  let result = [].concat(arr);
+  for (let i = 0; i < result.length; i++) {
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+    if (result[i] === '--discard-next') {
+      if (i < result.length - 1) result.splice(i + 1, 1);
+    }
+
+    else if (result[i] === '--discard-prev') {
+      if (i > 1) result.splice(i - 1, 1);
+    }
+
+    else if (result[i] === '--double-next') {
+      if (i < result.length - 1) result.splice(i, 1, result[i + 1])
+    }
+
+    else if (result[i] === '--double-prev') {
+      if (i > 1) result.splice(i, 1, result[i - 1]);
+    }
+  }
+  return result.filter(x => ['--double-prev', '--double-next', '--discard-prev', '--discard-next'].indexOf(x) == -1);
+}
